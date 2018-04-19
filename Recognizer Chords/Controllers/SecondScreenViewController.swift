5599e7320942d384ff61fbd8d1a91665cdbd8629
Luciano Schillagi <luko.web@gmail.com>
2018-04-19 10:29:50 -0300

    docs: Add the readme file edited

--- Recognizer Chords/Controllers/SecondScreenViewController.swift
+++ Recognizer Chords/Controllers/SecondScreenViewController.swift
@@ -6,30 +6,114 @@
 //  Copyright © 2018 luko. All rights reserved.
 //
 
+/* Controller */
+
 import UIKit
 
+/* Abstract:
+TODO: completar...
+*/
+
 class SecondScreenViewController: UIViewController {
+	
+	//*****************************************************************
+	// MARK: - Properties
+	//*****************************************************************
+	
+	// esconde la barra de estado
+	override var prefersStatusBarHidden: Bool {
+		return true
+	}
+	
+	
+	//*****************************************************************
+	// MARK: - IBOutlets
+	//*****************************************************************
+	
+	// los botones de los acordes..
+	@IBOutlet weak var majorButton: UIButton!
+	@IBOutlet weak var minorButton: UIButton!
+	@IBOutlet weak var diminishedButton: UIButton!
+	@IBOutlet weak var augmentedButton: UIButton!
+	
+	@IBOutlet weak var myLastScores: UIView!
+	
+	//*****************************************************************
+	// MARK: - Actions
+	//*****************************************************************
+	
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	@IBAction func playButtonPressed(_ sender: UIButton) {
+		
+		
+		
+	}
+	
+	@IBAction func majorButtonPressed(_ sender: UIButton) {
+		
+		
+	}
+	
+	@IBAction func minorButtonPressed(_ sender: UIButton) {
+		
+		
+	}
+	
+	@IBAction func diminishedButtonPressed(_ sender: UIButton) {
+		
+		
+	}
+	
+	@IBAction func augmentedButtonPressed(_ sender: UIButton) {
+		
+		
+		
+	}
+	
+	
+	// last 3 scores ////////////////////////////////////////////////////////
+	
+	@IBAction func openScoreMessage(_ sender: UIButton) {
+		
+		// emerge esta vista
+		self.myLastScores.isHidden = false
+		
+		// se esconde el botón de flecha para arriba anterior
+		self.upArrow.isHidden = true
+		
+	}
+	
+	@IBAction func closeScoreMessage(_ sender: UIButton) {
+		
+		
+	}
+	
+	@IBAction func lastScoresButton(_ sender: UIButton) {
+		
+		// lleva al 'LastScoresVC'
+		
+	}
+	
+	
 
-        // Do any additional setup after loading the view.
-    }
+	
+	
+	
+	
+	
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+	//*****************************************************************
+	// MARK: - Life Cycle
+	//*****************************************************************
+	override func viewDidLoad() {
+			super.viewDidLoad()
 
-    /*
-    // MARK: - Navigation
+			// Do any additional setup after loading the view.
+	}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	
+	
+	
+	
 
-}
+} // end class
