29e291fb5875d64f18adb3cd0bd60bf01e15b9b2
Luciano Schillagi <luko.web@gmail.com>
2018-06-28 01:19:15 -0300

    feat: Add autolayout in 'ChordsInfoSecondScreen'

--- Recognizer Chords/Autolayout/ChordInfoSecond_Autolayout.swift
+++ Recognizer Chords/Autolayout/ChordInfoSecond_Autolayout.swift
@@ -32,7 +32,7 @@ extension ChordsInfoSecondViewController {
 		dimLabel.translatesAutoresizingMaskIntoConstraints = false
 		dimNotationImage.translatesAutoresizingMaskIntoConstraints = false
 		dimInfoLabel.translatesAutoresizingMaskIntoConstraints = false
-		
+
 		// augmented
 		augView.translatesAutoresizingMaskIntoConstraints = false
 		augLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -45,35 +45,18 @@ extension ChordsInfoSecondViewController {
 		
 		NSLayoutConstraint.activate([
 			
-			
 			// major view constraints
 			majorView.topAnchor.constraint(equalTo: view.topAnchor),
 			majorView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-			majorView.trailingAnchor.constraint(equalTo: dimView.leadingAnchor),
+			majorView.trailingAnchor.constraint(equalTo: view.centerXAnchor),
 			majorView.bottomAnchor.constraint(equalTo: view.centerYAnchor),
 			
 			majorLabel.bottomAnchor.constraint(equalTo: majorNotationImage.topAnchor, constant: -25),
-			majorLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+			majorLabel.centerXAnchor.constraint(equalTo: majorView.centerXAnchor),
 			majorNotationImage.centerXAnchor.constraint(equalTo: majorView.centerXAnchor),
 			majorNotationImage.centerYAnchor.constraint(equalTo: majorView.centerYAnchor),
 			majorInfoLabel.topAnchor.constraint(equalTo: majorNotationImage.bottomAnchor, constant: 30),
-			majorInfoLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-			
-			
-			// minor view constraints
-			minorView.topAnchor.constraint(equalTo:view.centerYAnchor),
-			minorView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-			minorView.trailingAnchor.constraint(equalTo: augView.leadingAnchor),
-			minorView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-			
-			
-			minorLabel.bottomAnchor.constraint(equalTo: minorNotationImage.topAnchor, constant: -25),
-			minorLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-			minorNotationImage.centerXAnchor.constraint(equalTo: minorView.centerXAnchor),
-			minorNotationImage.centerYAnchor.constraint(equalTo: minorView.centerYAnchor),
-			minorInfoLabel.topAnchor.constraint(equalTo: minorNotationImage.bottomAnchor, constant: 30),
-			minorInfoLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-			
+			majorInfoLabel.centerXAnchor.constraint(equalTo: majorView.centerXAnchor),
 			
 			// diminished view constraints
 			dimView.topAnchor.constraint(equalTo:view.topAnchor),
@@ -82,12 +65,24 @@ extension ChordsInfoSecondViewController {
 			dimView.bottomAnchor.constraint(equalTo: view.centerYAnchor),
 			
 			dimLabel.bottomAnchor.constraint(equalTo: dimNotationImage.topAnchor, constant: -25),
-			dimLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+			dimLabel.centerXAnchor.constraint(equalTo: dimView.centerXAnchor),
 			dimNotationImage.centerXAnchor.constraint(equalTo: dimView.centerXAnchor),
 			dimNotationImage.centerYAnchor.constraint(equalTo: dimView.centerYAnchor),
 			dimInfoLabel.topAnchor.constraint(equalTo: dimNotationImage.bottomAnchor, constant: 30),
-			dimInfoLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-			
+			dimInfoLabel.centerXAnchor.constraint(equalTo: dimView.centerXAnchor),
+		
+			// minor view constraints
+			minorView.topAnchor.constraint(equalTo:view.centerYAnchor),
+			minorView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			minorView.trailingAnchor.constraint(equalTo: view.centerXAnchor),
+			minorView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+
+			minorLabel.bottomAnchor.constraint(equalTo: minorNotationImage.topAnchor, constant: -25),
+			minorLabel.centerXAnchor.constraint(equalTo: minorView.centerXAnchor),
+			minorNotationImage.centerXAnchor.constraint(equalTo: minorView.centerXAnchor),
+			minorNotationImage.centerYAnchor.constraint(equalTo: minorView.centerYAnchor),
+			minorInfoLabel.topAnchor.constraint(equalTo: minorNotationImage.bottomAnchor, constant: 30),
+			minorInfoLabel.centerXAnchor.constraint(equalTo: minorView.centerXAnchor),
 			
 			// augmented view constraints
 			augView.topAnchor.constraint(equalTo:view.centerYAnchor),
@@ -96,13 +91,11 @@ extension ChordsInfoSecondViewController {
 			augView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
 			
 			augLabel.bottomAnchor.constraint(equalTo: augNotationImage.topAnchor, constant: -25),
-			augLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+			augLabel.centerXAnchor.constraint(equalTo: augView.centerXAnchor),
 			augNotationImage.centerXAnchor.constraint(equalTo: augView.centerXAnchor),
 			augNotationImage.centerYAnchor.constraint(equalTo: augView.centerYAnchor),
-			dimInfoLabel.topAnchor.constraint(equalTo: augNotationImage.bottomAnchor, constant: 30),
-			dimInfoLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-			
-			
+			augInfoLabel.topAnchor.constraint(equalTo: augNotationImage.bottomAnchor, constant: 30),
+			augInfoLabel.centerXAnchor.constraint(equalTo: augView.centerXAnchor),
 			
 			
 			// close button constraints
@@ -111,15 +104,7 @@ extension ChordsInfoSecondViewController {
 			
 			])
 		
-		
-		
-		
 	}
-	
-	
-	
-	
-	
 	
 	
 }
